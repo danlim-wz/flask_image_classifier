30be1c4b5df9a1056b4f7d75df9109d9ab99ff0a
danlim-wz <limwz.daniel@gmail.com>
2019-07-07 22:45:43 +0800

    first commit

---
+++ image_classifier/__init__.py
@@ -0,0 +1,1 @@
+app.static_folder = 'static'
---
+++ image_classifier/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, make_response, render_template
+# import keras
+# import numpy as np
+# from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+# from keras.preprocessing.image import load_img, img_to_array
+import os, io
+from PIL import Image
+
+app = Flask(__name__, static_folder='C:/Users/Owner/Desktop/image_classifier/static')
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.config['IMAGE_UPLOADS'] = os.path.join(APP_ROOT, 'static')
+
+@app.route("/upload",methods=["GET","POST"])
+def classify_image():
+    if request.method == "POST":
+        image = request.files['input_file']
+        file_path = os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
+        # filename = image.filename
+        image_pil = Image.open(image)
+        # image_pil.thumbnail((600,300), Image.ANTIALIAS)
+        image_pil = image_pil.resize((500,300), Image.ANTIALIAS)
+        image_pil.save(file_path)     
+        # return 'image saved'
+        # image = load_img(image, target_size=(224, 224))
+        # image = img_to_array(image)
+        # image = np.expand_dims(image, axis=0)
+        # image = preprocess_input(image)
+        # prediction = resnet_model.predict(image)
+        # prediction = decode_predictions(prediction)[0][0][1]
+        # prediction = prediction.replace('_',' ')
+        # return make_response('Predicted: '+str(prediction))
+        # return render_template("upload_button.html")      
+        return render_template("upload_button.html", image_path = image.filename, predict = 'test')
+    # full_name = os.path.join(app.config['IMAGE_UPLOADS'], 'lion.jpg')
+    # return render_template("upload_button.html", image_path = 'landing_page_pic.jpg')
+    return render_template("upload_button.html", image_path = 'landing_page_pic.jpg')
+
+if __name__ == '__main__':
+    # resnet_model = ResNet50(weights='imagenet')
+    app.run(host='127.0.0.1', debug=True, port=8000)
