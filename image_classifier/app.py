c1ca4a6bf598a02f18c112b60e6285572375be0c
danlim-wz <limwz.daniel@gmail.com>
2019-07-08 21:30:47 +0800

    completed html interface

--- image_classifier/app.py
+++ image_classifier/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, make_response, render_template
-# import keras
-# import numpy as np
-# from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-# from keras.preprocessing.image import load_img, img_to_array
+import keras
+import numpy as np
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.preprocessing.image import load_img, img_to_array
 import os, io
 from PIL import Image
 
@@ -15,27 +15,27 @@ app.config['IMAGE_UPLOADS'] = os.path.join(APP_ROOT, 'static')
 def classify_image():
     if request.method == "POST":
         image = request.files['input_file']
-        file_path = os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
-        # filename = image.filename
+        filename = image.filename
+        file_path = os.path.join(app.config["IMAGE_UPLOADS"], filename)
         image_pil = Image.open(image)
-        # image_pil.thumbnail((600,300), Image.ANTIALIAS)
-        image_pil = image_pil.resize((500,300), Image.ANTIALIAS)
+        image_pil.thumbnail((600,300), Image.ANTIALIAS)
+        # image_pil = image_pil.resize((500,300), Image.ANTIALIAS)
         image_pil.save(file_path)     
         # return 'image saved'
-        # image = load_img(image, target_size=(224, 224))
-        # image = img_to_array(image)
-        # image = np.expand_dims(image, axis=0)
-        # image = preprocess_input(image)
-        # prediction = resnet_model.predict(image)
-        # prediction = decode_predictions(prediction)[0][0][1]
-        # prediction = prediction.replace('_',' ')
+        image = load_img(image, target_size=(224, 224))
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        image = preprocess_input(image)
+        prediction = resnet_model.predict(image)
+        prediction = decode_predictions(prediction)[0][0][1]
+        prediction = prediction.replace('_',' ')
         # return make_response('Predicted: '+str(prediction))
         # return render_template("upload_button.html")      
-        return render_template("upload_button.html", image_path = image.filename, predict = 'test')
+        return render_template("upload_button.html", image_path = filename, prediction = 'Prediction: '+prediction)
     # full_name = os.path.join(app.config['IMAGE_UPLOADS'], 'lion.jpg')
     # return render_template("upload_button.html", image_path = 'landing_page_pic.jpg')
     return render_template("upload_button.html", image_path = 'landing_page_pic.jpg')
 
 if __name__ == '__main__':
-    # resnet_model = ResNet50(weights='imagenet')
-    app.run(host='127.0.0.1', debug=True, port=8000)
+    resnet_model = ResNet50(weights='imagenet')
+    app.run(host='127.0.0.1', debug=False, threaded=False, port=8000)
